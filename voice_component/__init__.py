a93ccc1ec07ebd0100d7327425b4cd62db5adacf
Agita Sonia Butar Butar <agitasonia2508@gmail.com>
2026-08-14 17:44:25 +0700

    Update __init__.py

--- voice_component/__init__.py
+++ voice_component/__init__.py
@@ -1,11 +1,9 @@
 import streamlit.components.v1 as components
 
-
 _component = components.declare_component(
     "voice_recognition",
     path="voice_component/frontend"
 )
-
 
 def voice_recognition(
     speak_text="",
